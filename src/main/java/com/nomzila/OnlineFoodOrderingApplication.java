d63448b8e47f48e8714bb517e72fe88afe66c0db
AbhishekMishra <abhishekmishra968517@gmail.com>
2025-08-16 11:39:07 +0530

    hello this is first commit

--- src/main/java/com/nomzila/OnlineFoodOrderingApplication.java
+++ src/main/java/com/nomzila/OnlineFoodOrderingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineFoodOrderingApplication {
 	public static void main(String[] args) {
-		System.out.println("Rishi Mishra");
+		System.out.println("Rishi");
 		SpringApplication.run(OnlineFoodOrderingApplication.class, args);
 	}
 }
